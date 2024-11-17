8b531f1df35bd73dcb5d81ee308bdbe1ff0ec052
Andrew.Worley <drewjw1212@gmail.com>
2024-11-16 20:11:56 -0500

    final project

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ const app = express();
 const { PORT = 3001 } = process.env;
 
 mongoose.connect(mongoServerAddress);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
